bdc63d80cc14ea1ef16c50619bdb4ab5467a6296
Abdelrahman Nader <abdunader1772005@gmail.com>
2025-01-30 15:10:22 +0200

    Basketball Scoreboard files

---
+++ 8 - Basketball Scoreboard/index.js
@@ -0,0 +1,12 @@
+function addOne(id) {
+  let element = document.getElementById(id);
+  element.textContent = parseInt(element.textContent) + 1;
+}
+function addTwo(id) {
+  let element = document.getElementById(id);
+  element.textContent = parseInt(element.textContent) + 2;
+}
+function addThree(id) {
+  let element = document.getElementById(id);
+  element.textContent = parseInt(element.textContent) + 3;
+}
